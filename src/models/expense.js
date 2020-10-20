974787745a3e7cf7958465b0241e5666189c6b2e
ChiaHsing <mac@Macde-MacBook-Air.local>
2020-10-21 00:49:52 +0800

    set express server and mongoose schema

--- src/models/expense.js
+++ src/models/expense.js
@@ -1,12 +1,25 @@
 const mongoose = require('mongoose')
 
 const expenseSchema = new mongoose.Schema({
+    user: {
+        type: String,
+        required: true,
+        trim: true,
+    },
     expense: {
         type: String,
         required: true,
     },
     amount: {
         type: Number,
+        required: true,
+    },
+    date: {
+        type: Date,
+        require: true,
+    },
+    category: {
+        type: String,
         required: true,
     },
 })
